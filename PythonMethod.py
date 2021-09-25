6b41a7abf4803562a8cf908de0528f7f26799ea1
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2021-09-25 20:18:31 +0530

    Create PythonMethod.py

---
+++ PythonMethod.py
@@ -0,0 +1,40 @@
+l1=list((10,20,30,40))
+l2=list((50,60,70,80))
+print(cmp(l1,l1))
+print(min(l1))
+print(max(l1))
+z=list("hello")
+print z
+'''
+Method	Description
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()	Sorts the list
+Sr.No.	Methods with Description
+1	list.append(obj)
+Appends object obj to list
+2	list.count(obj)
+Returns count of how many times obj occurs in list
+3	list.extend(seq)
+Appends the contents of seq to list
+4	list.index(obj)
+Returns the lowest index in list that obj appears
+5	list.insert(index, obj)
+Inserts object obj into list at offset index
+6	list.pop(obj=list[-1])
+Removes and returns last object or obj from list
+7	list.remove(obj)
+Removes object obj from list
+8	list.reverse()
+Reverses objects of list in place
+9	list.sort([func])
+Sorts objects of list, use compare func if given
+'''
