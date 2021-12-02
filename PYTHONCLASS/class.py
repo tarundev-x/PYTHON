96040d49364036d08e11eff21dc34d83bfd8a3e6
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2021-12-02 09:59:06 +0530

    Create class.py

---
+++ PYTHONCLASS/class.py
@@ -0,0 +1,13 @@
+class mobile:
+    pass
+mob1=mobile()
+mob2=mobile()#creating object
+
+#creating attributes
+mob1.price=20000
+mob1.brand="apple"
+mob1.version=10
+#accessing the attributes
+mob1.version=11
+print(mob1.price,mob1.brand,mob1.version)
+
