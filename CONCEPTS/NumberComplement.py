08c69a4333b3bb97c26258171b23ab1ee748ceb1
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2022-01-29 18:43:18 +0530

    Update NumberComplement.py

--- CONCEPTS/NumberComplement.py
+++ CONCEPTS/NumberComplement.py
@@ -1,4 +1,9 @@
   def findComplement(self, num: int) -> int:
+   # 1. int.bit_length()
+  #Returns the number of bits required to represent an integer in binary, excluding the sign and leading zeros.
         num_bits=num.bit_length()
+    '''a = 5 = 0000 0101 (Binary)
+       a << 1 = 0000 1010 = 10'''
+
         mask=(1<<num_bits)-1
         return num^mask
